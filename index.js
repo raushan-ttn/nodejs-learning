cf2c0e706e7d489efe08207851c15882665b2190
Raushan Tiwari <raushan.tiwari@tothenew.com>
2022-12-31 20:22:24 +0530

    api/server

--- index.js
+++ index.js
@@ -2,11 +2,78 @@
 // console.log(hello);
 
 const fs = require("fs");
+const http = require("http");
+const url = require("url");
 
-const textIn = fs.readFileSync("./txt/input.txt",'utf-8');
-console.log(textIn);
+/////////////////////////////////////
+// FILES
 
-const textOut = `This is what we know about avacado: ${textIn} \n Created ${Date.now()}`;
+// Blocking code. Synchronous way.
+/*
+    const textIn = fs.readFileSync("./txt/input.txt",'utf-8');
+    console.log(textIn);
 
-fs.writeFileSync('./txt/input.txt',textOut);
-console.log("File written");
+    const textOut = `This is what we know about avacado: ${textIn} \n Created ${Date.now()}`;
+
+    fs.writeFileSync('./txt/input.txt',textOut);
+    console.log("File written");
+ */
+
+// Non-Blocking code. ASynchronous way.
+/*
+    fs.readFile("./txt/start.txt", 'utf-8', (err, data) => {
+        if (err) {
+            console.log("Error!!!" + err);
+            return;
+        }
+        fs.readFile(`./txt/${data}.txt`, 'utf-8', (err1, data1) => {
+            console.log(data1);
+            fs.readFile("./txt/append.txt", 'utf-8', (err2, data2) => {
+                console.log(data2);
+                fs.writeFile("./txt/final.txt", `${data1}\n${data2}`, 'utf-8', err => {
+                    console.log("Our file written");
+                });
+            });
+        });
+    });
+
+    console.log("We are reading files");
+
+ */
+
+// Note: If we get error during file read then will not execute below lines.otherwise works fine.
+
+/////////////////////////////////////
+// SERVERS
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+    const pathName = req.url;
+    if (pathName === '/' || pathName === '/overview') {
+        res.end("This is OVERVIEW!!!");
+    } else if (pathName === '/product') {
+        res.end("This is PRODUCT!!!");
+    } else if (pathName === '/api') {
+        res.writeHead(200, {
+            "Content-type": "application/json"
+        });
+        res.end(data);
+    }
+    else {
+        res.writeHead(404, {
+            "Content-type": "text-html",
+            'MyOwn-header': 'Hello world'
+        });
+        res.end("<h1>Page Not Found !!!</h1>");
+    }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log("listing nodeJs server from 8000")
+});
+
+// Note: console.log output will show in terminal not browser console.
+// We can create multiple Route based on conditions in NodeJs. (this not belongs to directory path like /templates).
+// http.createServer call every page load. So if we use readFile inside "/api" then file will be read every page load. So we readFile once (synchronous way) at the top of the file. this will call only once.
+//
---
+++ my-readMe.txt
@@ -0,0 +1,15 @@
+Synchronous Code: It executes code line by line, which means that each line of code must wait for the previous code line to resolve. this can be problem specially with slow operation, each line blocks the execution of rest of the code. So we say Synchronous code is also called blocking code.
+
+Asynchronous Code: is also called non blocking code. we offload heavy work to background.then the works get done, a callback function register before is called to handle the result, and during all the time rest of the code is executing.
+
+Diffrence between normal function vs arrow function
+
+function(){
+
+}
+
+const data = () => {
+
+}
+1. Arrow function does not have own this keyword, it has parent this, this is called laxical scope. And normal function have own this keyword.
+
