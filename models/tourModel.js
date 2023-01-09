77fbd8c761a8150b2ce16aa63b92cd083c114c71
Raushan Tiwari <raushan.tiwari@tothenew.com>
2023-01-09 23:37:25 +0530

    Modal update

--- models/tourModel.js
+++ models/tourModel.js
@@ -4,17 +4,59 @@ const mongoose = require('mongoose');
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'A tour must have name.'], // this is validator and we can also create our own.
+    required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true,
+    maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+    minlength: [10, 'A tour name must have more or equal then 10 characters'],
+    // validate: [validator.isAlpha, 'Tour name must only contain characters']
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+    enum: {
+      values: ['easy', 'medium', 'difficult'],
+      message: 'Difficulty is either: easy, medium, difficult',
+    },
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+    min: [1, 'Rating must be above 1.0'],
+    max: [5, 'Rating must be below 5.0'],
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
-    required: [true, 'A tour must have price'],
+    required: [true, 'A tour must have a price'],
   },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a description'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  startDates: [Date],
+
 });
 
 // Create modal (Tour), variableName and modal name always Uppercase.
