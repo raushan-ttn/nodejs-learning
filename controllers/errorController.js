fcd5d7eecd37ecc84fb21d86e28ecf3b69621a3c
Raushan Tiwari <raushan.tiwari@tothenew.com>
2023-01-13 21:53:22 +0530

    Error based on instance

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,10 +1,19 @@
 module.exports = (err, req, res, next) => {
-  console.log(err.stack); // This will display all stack of error like debugger.
+  // console.log(err.stack); // This will display all stack of error like debugger.
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  // ERROR: show error as per instance and this will show in postman.
+  if (process.env.NODE_ENV === 'development') {
+    res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack,
+    });
+  } else if (process.env.NODE_ENV === 'production') {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
 };
