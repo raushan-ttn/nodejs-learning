dfd80fe31f6e6fcfbc98261c1cf0b7f764dc7755
Raushan Tiwari <raushan.tiwari@tothenew.com>
2023-01-15 13:40:49 +0530

    package: jsonwebtoken

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,12 @@ const catchAsync = require('../utils/catchAsync');
 // catchAsync() function accept async function and handle catch(). so that we don't
 // need to add catch() here.
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    confirmpassword: req.body.confirmpassword,
+  });
 
   res.status(201).json({
     status: 'success',
